65cef4b84ebe59641f4532add01d3a038345395d
okandredaniel <andre.fervilha@gmail.com>
2024-08-07 11:57:53 +0100

    simplify configs

--- config.py
+++ config.py
@@ -1,35 +1,10 @@
 config = {
-    'MACHINE': 'Embalagem 1',
-
-    #SERVER ---------------------------------------------_
-    'URL': 'https://erp.nuvensvivazes.com',
-    #'URL': 'http://localhost:8069',
-    'HEADERS': {'Content-type': 'application/json', "Accept": "application/json"},
-    "DB": "nuvens-master-5712383",
-    #"DB": "nuv",
-
-    # AUTH ----------------------------------------------
-    "LOGIN": "almofadas1_tablet",
-    "PASSWORD": "`(4]Py#{.*HT;'(x",
-    'AUTH_URL': '/web/session/authenticate',
-
-    #LONGPOLLING ---------------------------------------
-
-    'POLL': '/longpolling/poll',
-    'CHANNELS': ['factory'],
-    'LONG_IS_ACTIVE': True,
-    'LONGPOLLING_LAST': 0,
-
-    #DEVICE ---------------------------------------------
-    'DEVICE_URL': '/mrp/opc-ua/vals',
-    'SET_DEVICE_URL': '/mrp/opc-ua/set-vals',
-
-    #OPC-UA CLIENT --------------------------------------
-    #'OPC_SERVER': "opc.tcp://arxibox:53530/OPCUA/SimulationServer"
-    'OPC_SERVER': "opc.tcp://192.168.1.150:4840"
+    "MACHINE": "Embalagem 1",
+    "DEVICE_URL": "/mrp/opc-ua/vals",
+    "SET_DEVICE_URL": "/mrp/opc-ua/set-vals",
+    "OPCUA_SERVER_ENDPOINT": "opc.tcp://192.168.1.150:4840",
+    "MAX_QUEUE_RETRIES": 3,
+    "CLIENT_HOST": "0.0.0.0",
+    "CLIENT_PORT": 5001,
+    "CLIENT_ENTRY_POINT": "/receive_recipe",
 }
-
-
-
-
-
