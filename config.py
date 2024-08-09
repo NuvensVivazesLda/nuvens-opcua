09322ef04b2c486b16175a14f63f2da59562aba9
okandredaniel <andre.fervilha@gmail.com>
2024-08-09 10:48:56 +0100

    update server ip

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ config = {
     "MACHINE": "Embalagem 1",
     "DEVICE_URL": "/mrp/opc-ua/vals",
     "SET_DEVICE_URL": "/mrp/opc-ua/set-vals",
-    "OPCUA_SERVER_ENDPOINT": "opc.tcp://192.168.1.150:4840",
+    "OPCUA_SERVER_ENDPOINT": "opc.tcp://192.168.2.150:4840",
     "MAX_QUEUE_RETRIES": 3,
     "CLIENT_HOST": "0.0.0.0",
     "CLIENT_PORT": 5001,
